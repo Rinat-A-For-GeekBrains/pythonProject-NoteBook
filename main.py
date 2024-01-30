3d398ff1045cb996ac308b4ce12f5e93dec30a54
Rinat <alimbekovrl@gmail.com>
2024-01-31 01:29:28 +0500

    2 commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import Controller as c
+
+c.run()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
